7edc4ca1b85a80fb9391207c75723e9ef5256a20
lzh <52725224+Yourdaylight@users.noreply.github.com>
2021-04-09 18:03:59 +0800

    前后端启动脚本(仅支持linux)

--- Auto_maching_learning/settings.py
+++ Auto_maching_learning/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',  # 数据库引擎
         'NAME': 'AML',  # 你要存储数据的库名，事先要创建之
         'USER': 'root',  # 数据库用户名
-        'PASSWORD': '123456',  # 密码
+        'PASSWORD': 'LZHlzh.root123',  # 密码
         'HOST': 'localhost',  # 主机
         'PORT': '3306',  # 数据库使用的端口
     },
---
+++ run_start.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+# @Time    :2021/4/9 17:50
+# @Author  :lzh
+# @File    : run_start.py
+# @Software: PyCharm
+import os
+import threading
+
+
+class myThread(threading.Thread):
+    def __init__(self, cmd):
+        threading.Thread.__init__(self)
+        self.cmd = cmd
+
+    def run(self):
+        os.system(self.cmd)
+
+
+# 创建新线程
+start_backend = "python3 manage.py runserver"
+start_fronend = "cd ../AML_frontend/ && npm run dev"
+thread_backend = myThread(start_backend)
+thread_frontend = myThread(start_fronend)
+
+# 开启新线程
+thread_backend.start()
+thread_frontend.start()
